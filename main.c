893714dba3686cf0d6d2be9a630baaab0e3ce1ae
Beka Buachidze <97060054+MortuoRege@users.noreply.github.com>
2025-09-18 20:04:10 +0400

    initial design of malloc

--- main.c
+++ main.c
@@ -1,13 +1,62 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdbool.h>
+#define ALIGN8(x) ((x + 7) & ~7)
+
+
+struct __attribute__((aligned(8))) block
+{
+    size_t size;
+    bool is_free;
+    struct block* next;
+};
+static struct block* head = NULL;
+
+void* my_malloc(size_t size)
+{
+    size_t aligned_size = ALIGN8(size);
+    void* ptr = NULL;
+    if(head == NULL)
+    {
+        ptr = sbrk(aligned_size + sizeof(struct block));
+        if (ptr == (void*)-1) return NULL;
+        struct block* b = (struct block*)ptr;
+        b->size = aligned_size;
+        b->is_free = false;
+        b->next = NULL;
+        head = b;
+        return (void*)((char*)ptr + sizeof(struct block));
+    }
+    else {
+    {
+        struct block* prev = NULL;
+        struct block* current = head;
+        while(current != NULL)
+        {
+            if(current->is_free && (current->size >= aligned_size))
+            {
+                current->is_free = false;
+                return (void*)((char*)current + sizeof(struct block));
+            }
+            prev = current;
+            current = current->next;
+        }
+        struct block* new_block = sbrk(aligned_size + sizeof(struct block));
+        if (new_block == (void*)-1) return NULL;
+        struct block* b = (struct block*)new_block;
+        b->size = aligned_size;
+        b->is_free = false;
+        b->next = NULL;
+        prev->next = b;
+        return (void*)((char*)b + sizeof(struct block));
+
+    }
+}
+
 
 int main() {
-    void* old_brk = sbrk(0);            // current break
-    printf("Old break: %p\n", old_brk);
-
-    void* new_brk = sbrk(4096);         // move up by 4 KB
-    printf("Allocated 4 KB at: %p\n", old_brk);  // usable memory starts here
-    printf("New break: %p\n", sbrk(0));
-
+    int *p = (int*)my_malloc(sizeof(int));
+    *p = 42;
+    printf("Allocated memory: %d\n", *p);
     return 0;
 }
